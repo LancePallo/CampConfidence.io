8eaca567940dc7f95a88c43cc24d46ea18f6cc8c
lance <lancepallo2000@gmail.com>
2024-07-30 14:55:20 -0500

    app.js revert

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import LoginScreen from './screens/LoginScreen';
-import WeekScreen from './screens/WeekScreen';
-import DayScreen from './screens/DayScreen';
-import ChoiceScreen from './screens/ChoiceScreen';
-import CheckInOutScreen from './screens/CheckInOutScreen';
-import RosterScreen from './screens/RosterScreen';
+import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = (username, password) => {
-    if (username === 'BestCamp' && password === '1665') {
-      setIsLoggedIn(true);
-    } else {
-      alert('Invalid credentials');
-    }
-  };
-
+function App() {
   return (
-    <div className="container">
-      <Router>
-        <Switch>
-          {isLoggedIn ? (
-            <>
-              <Route path="/week">
-                <WeekScreen setIsLoggedIn={setIsLoggedIn} />
-              </Route>
-              <Route path="/day" component={DayScreen} />
-              <Route path="/choice" component={ChoiceScreen} />
-              <Route path="/check-in-out" component={CheckInOutScreen} />
-              <Route path="/roster" component={RosterScreen} />
-              <Redirect from="/" to="/week" />
-            </>
-          ) : (
-            <Route path="/">
-              <LoginScreen onLogin={handleLogin} />
-            </Route>
-          )}
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
